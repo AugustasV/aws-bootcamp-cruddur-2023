e01f638edce80f325198ec9b8f78e4f013cfaae7
Augustas <reg1nt1z@gmail.com>
2023-05-19 10:06:09 +0000

    Configure devcontainer/codespaces environments

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -43,13 +43,13 @@ import rollbar.contrib.flask
 from flask import got_request_exception
 
 # Configuring Logger to Use CloudWatch
-LOGGER = logging.getLogger(__name__)
-LOGGER.setLevel(logging.DEBUG)
-console_handler = logging.StreamHandler()
-cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
-LOGGER.addHandler(console_handler)
-LOGGER.addHandler(cw_handler)
-LOGGER.info("App logs")
+# LOGGER = logging.getLogger(__name__)
+# LOGGER.setLevel(logging.DEBUG)
+# console_handler = logging.StreamHandler()
+# cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
+# LOGGER.addHandler(console_handler)
+# LOGGER.addHandler(cw_handler)
+# LOGGER.info("App logs")
 
 # this is for error logging in AWS X-ray
 # @app.after_request
